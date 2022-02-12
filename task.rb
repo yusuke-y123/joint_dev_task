6babfa22bd2e67190b7cb729a93304eaef445d46
YUSUKE YAMAZAKI <70199057+yusuke-y123@users.noreply.github.com>
2022-02-12 18:26:28 +0900

    Q5~10まで提出します。

--- task.rb
+++ task.rb
@@ -50,6 +50,8 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
+  numbers2 = numbers1.map {|n| n * 10}
+  p numbers2 
 
 end
 
@@ -57,6 +59,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+  array.map!(&:to_i)
 
   # 以下は変更しないで下さい
   p array
@@ -66,6 +69,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
@@ -76,6 +81,7 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  puts names.map.with_index(1){|names, i| "会員No.#{i} #{names}さん"}
 
 end
 
@@ -83,7 +89,13 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+    if food.include?("うに")
+      puts "好物です"
+    else
+      puts "まぁまぁ好きです"
+    end
+  end
 end
 
 def q11
